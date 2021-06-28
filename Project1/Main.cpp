56960f412125180918d0a9b06c5b6ca4edca35ba
Осадчий Кирилл <kirillok21@yandex.ua>
2021-06-28 19:49:09 +0300

    Коммент на не нужные строки

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -14,17 +14,17 @@ int main()
 {
 
 	std::cout << "data() = " << data.atoms()->items()->data()->get()->atom_type() << " size = " << data.atoms()->items()->size() << std::endl;
-	data.atoms()->items()->data()->get()->body()->_io()->seek(0);
-	std::cout << "pos " << data.atoms()->items()->data()->get()->body()->_io()->pos() << std::endl;
-	std::cout << data.atoms()->items()->data()->get()->_io()->read_bytes_full() << std::endl << std::endl;
+	//data.atoms()->items()->data()->get()->body()->_io()->seek(0);
+	//std::cout << data.atoms()->items()->data()->get()->_io()->read_bytes_full() << std::endl << std::endl;
 
 	std::cout << "back() = " << data.atoms()->items()->back().get()->atom_type() << " size = " << data.atoms()->items()->back().get()->_io()->size() << std::endl;
-	data.atoms()->items()->back()->body()->_io()->seek(0);
-	std::cout << data.atoms()->items()->back().get()->body()->_io()->read_bytes_full() << std::endl << std::endl;
+	//data.atoms()->items()->back()->body()->_io()->seek(0);
+	//std::cout << data.atoms()->items()->back().get()->body()->_io()->read_bytes_full() << std::endl << std::endl;
 
 	std::cout << "_root() = " << data.atoms()->_root()->atoms()->items()->data()->get()->atom_type() << " size = " << data.atoms()->_root()->atoms()->items()->data()->get()->body()->_io()->size() << std::endl;
-	data.atoms()->items()->data()->get()->body()->_io()->seek(0);
-	std::cout << data.atoms()->_root()->atoms()->items()->data()->get()->body()->_io()->read_bytes_full() << std::endl;
+	//data.atoms()->items()->data()->get()->body()->_io()->seek(0);
+	//std::cout << data.atoms()->_root()->atoms()->items()->data()->get()->body()->_io()->read_bytes_full() << std::endl;
+	
 	//std::cout << data.atoms()->_io()->read_bytes_full(); // full file
 
 	_getch();
